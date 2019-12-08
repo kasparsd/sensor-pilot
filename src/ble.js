c0830fcfbca568b03bc4f98bd498fbd691546c61
Kaspars Dambis <hi@kaspars.net>
2019-12-08 23:39:10 +0200

    Read sensor data

---
+++ src/ble.js
@@ -0,0 +1,52 @@
+export default class Ble {
+
+  constructor(bluetoothApi, deviceOptions) {
+    this.bluetoothApi = bluetoothApi
+    this.deviceOptions = deviceOptions
+
+    this.server = null
+    this.device = null;
+  }
+
+  isConnected() {
+    return (this.server && this.server.connected)
+  }
+
+  getDevice() {
+    return this.bluetoothApi.requestDevice(this.deviceOptions)
+      .then(device => device.gatt.connect());
+  }
+
+  getGATTServer() {
+    if (this.isConnected()) {
+      return Promise.resolve(this.server)
+    }
+
+    return this.getDevice()
+      .then(server => {
+        this.server = server
+
+        return server
+      })
+  }
+
+  serviceCharacteristics(serviceUuid, characteristicResolvers) {
+    const characteristicUuids = Object.keys(characteristicResolvers)
+      .map(name => BluetoothUUID.getCharacteristic(name))
+
+    return this.getGATTServer()
+      .then(server => server.getPrimaryService(serviceUuid))
+      .then(service => service.getCharacteristics())
+      .then(characteristics => {
+        return Promise.all(
+          characteristics
+            .filter(characteristic => characteristicUuids.includes(characteristic.uuid))
+            .map(characteristic => characteristic.readValue())
+        )
+      })
+      .then(values => Object.values(characteristicResolvers)
+        .map((resolver, index) => resolver(values[index]))
+      )
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,40 +1,80 @@
+import "@babel/polyfill";
 import './styles/main.scss'
+import Ble from './ble'
 
-import ble from './ble'
+const decoder = new TextDecoder('utf-8')
+
+const SENSOR_SERVICE_UUID = 'f0cd1400-95da-4f4b-9ac8-aa55d312af0c'
+
+const aranetServices = {
+
+  sensor: {
+    serviceUuid: SENSOR_SERVICE_UUID,
+    resolvers: {
+      'f0cd3001-95da-4f4b-9ac8-aa55d312af0c': (value) => {
+        return {
+          co2: value.getUint16(0, true),
+          temperature: value.getUint16(2, true) / 20,
+          pressure: value.getUint16(4, true) / 10,
+          humidity: value.getUint8(6),
+          battery: value.getUint8(7),
+        }
+      }
+    }
+  },
+
+  device: {
+    serviceUuid: 'device_information',
+    resolvers: {
+      'manufacturer_name_string': (value) => decoder.decode(value),
+      'model_number_string': (value) => decoder.decode(value),
+      'serial_number_string': (value) => decoder.decode(value),
+      'hardware_revision_string': (value) => decoder.decode(value),
+      'software_revision_string': (value) => decoder.decode(value),
+    }
+  }
+
+}
+
+const ble = new Ble(
+  navigator.bluetooth,
+  {
+    filters: [
+      {
+        services: [SENSOR_SERVICE_UUID]
+      }
+    ],
+    optionalServices: [
+      'device_information',
+      'battery_service',
+    ],
+  }
+)
+
+async function connect() {
+  const sensorValues = await ble.serviceCharacteristics(
+    aranetServices.sensor.serviceUuid,
+    aranetServices.sensor.resolvers
+  )
+
+  document.querySelectorAll('.aranet-sensor-data .sensor-value').forEach(node => {
+    if (node.dataset.value in sensorValues[0]) {
+      node.textContent = sensorValues[0][node.dataset.value]
+    }
+  })
+}
+
+if ('bluetooth' in navigator) {
+  document.querySelector('#ble-connect')
+    .addEventListener('click', () => {
+      connect();
+    });
+} else {
+  // Display browser compatability warning.
+}
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js');
   });
-}
-
-function connect() {
-  navigator.bluetooth.requestDevice({
-    filters: [
-      {
-        namePrefix: 'Aranet',
-      },
-    ],
-  }).then((device) => {
-    return device.gatt.connect();
-  }).then(function(server) {
-    return server.getPrimaryService(0xf0cd140095da4f4b9ac8aa55d312af0c);
-  }).then(function(service) {
-    console.log('service', service)
-    return service.getCharacteristic(0xf0cd300195da4f4b9ac8aa55d312af0c);
-  }).then(function(characteristic) {
-    console.log('characteristic', characteristic);
-  }).catch(function(error) {
-      console.error('Connection failed!', error);
-  });
-}
-
-if ('bluetooth' in navigator) {
-  document.querySelector('#ble-connect').addEventListener(
-    'click',
-    event => {
-      connect();
-    });
-} else {
-  // Display browser compatability warning.
 }
