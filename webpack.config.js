31db2ea922bd1fffeebd0958f26627d64f4a289d
Kaspars Dambis <hi@kaspars.net>
2019-12-29 17:17:16 +0200

    Pre-cache the HTML file too

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const serviceWorkerExclude = []
+module.exports = (env, options) => {
+  const serviceWorkerExclude = []
 
-// Don't cache HTML files to enable hot-reloading.
-if (process.env.NODE_ENV !== 'production') {
-  serviceWorkerExclude.push(/\.html$/)
-}
+  // Don't cache HTML files to enable hot-reloading.
+  if (options.mode !== 'production') { // Is there a better way of checking for the "mode" flag?
+    serviceWorkerExclude.push(/\.html$/)
+  }
 
-module.exports = {
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+  return {
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'src/template.html',
+      }),
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        exclude: serviceWorkerExclude,
+      }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/template.html',
-    }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      exclude: serviceWorkerExclude,
-    }),
-  ],
+  }
 }
