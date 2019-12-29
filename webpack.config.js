dbf1abe9b1b98b9851d7ac3a91c1ca69046258b5
Kaspars Dambis <hi@kaspars.net>
2019-12-29 19:33:29 +0200

    Copy assets on build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = (env, options) => {
   const serviceWorkerExclude = []
@@ -47,6 +48,12 @@ module.exports = (env, options) => {
         offlineGoogleAnalytics: true,
         exclude: serviceWorkerExclude,
       }),
+      new CopyPlugin([
+        {
+          from: 'src/assets/',
+          to: 'assets/',
+        },
+      ]),
     ],
   }
 }
