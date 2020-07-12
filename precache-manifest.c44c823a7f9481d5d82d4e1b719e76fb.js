1ce3c695c2b6afa71b9d2dac0a51c1018909b4f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-12 12:13:37 +0000

    Deploy kasparsd/sensor-pilot to github.com/kasparsd/sensor-pilot.git:gh-pages

---
+++ precache-manifest.c44c823a7f9481d5d82d4e1b719e76fb.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b3192613808129ea9e47f6b1569417",
+    "url": "index.html"
+  },
+  {
+    "revision": "6b2711751aab07c91481",
+    "url": "main.css"
+  },
+  {
+    "revision": "6b2711751aab07c91481",
+    "url": "main.f4994a1cf13354f3d8a0.js"
+  }
+]);
